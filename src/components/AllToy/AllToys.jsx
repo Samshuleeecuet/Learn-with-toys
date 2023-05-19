addd4d674393016667d6742c0c85727b14d1d687
Samshuleeecuet <samshuleeeweb@gmail.com>
2023-05-19 20:53:28 +0600

    category section create and done

--- src/components/AllToy/AllToys.jsx
+++ src/components/AllToy/AllToys.jsx
@@ -10,31 +10,6 @@ const AllToys = () => {
     //console.log(toys)
     return (
         <div>
-            {/* <div className='grid mt-12 lg:grid-cols-3 pl-5 pr-5 gap-4'>
-                {
-                    toys?.map(toy=> 
-                        <div className="card w-[450px] glass" key={toy?._id}>
-                            <figure><img className='w-96 h-52' src={toy?.picture} alt="car!"/></figure>
-                            <div className="card-body">
-                                <h2 className="card-title">{toy?.name}</h2>
-                                { toy?.seller_name && 
-                                    <p>Seller: <span>{toy?.seller_name}</span></p>
-                                }
-                                <p>Category: {toy?.subcategory}</p>
-                                <div className='flex'>
-                                <p>Price: {toy?.price}</p>
-                                <p>Available Quantity: {toy?.quantity}</p>
-                                </div>
-                                <div className="card-actions justify-end">
-                                <button className="btn btn-primary">View Details</button>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        )
-                }
-            </div> */}
-
 
 <div className="overflow-x-auto w-full">
   <table className="table w-full">
--- src/components/Home/Category.jsx
+++ src/components/Home/Category.jsx
@@ -1,13 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const Category = () => {
     const [category,setCategory] = useState('science-kit')
+    const [toys,setAlltoys] = useState([])
     const handleCategory = (value)=>{
         setCategory(value)
-        console.log(value)
     }
+    useEffect(()=>{
+        fetch(`http://localhost:5000/alltoy/${category}`)
+        .then(res=> res.json())
+        .then(data=> setAlltoys(data))
+    },[category])
+
+    const categories = <>
+     <div className='grid mt-12 lg:grid-cols-3 pl-5 pr-5 gap-4'>
+                {
+                    toys?.map(toy=> 
+                        <div className="card w-[450px] glass" key={toy?._id}>
+                            <figure><img className='w-96 h-52' src={toy?.picture} alt="car!"/></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{toy?.name}</h2>
+                                { toy?.seller_name && 
+                                    <p>Seller: <span>{toy?.seller_name}</span></p>
+                                }
+                                <p>Category: {toy?.subcategory}</p>
+                                <div className='flex'>
+                                <p>Price: {toy?.price}</p>
+                                <p>Available Quantity: {toy?.quantity}</p>
+                                </div>
+                                <div className="card-actions justify-end">
+                                <button className="btn btn-primary">View Details</button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        )
+                }
+            </div> 
+    
+    </>
+
+
     return (
         <div className='font-serif mt-12'>
             <div>
@@ -23,13 +58,13 @@ const Category = () => {
                 </TabList>
 
                 <TabPanel className='border-t-2'>
-                <h2>Science kits</h2>
+                {categories}
                 </TabPanel>
                 <TabPanel>
-                <h2>Any content 2</h2>
+                {categories}
                 </TabPanel>
                 <TabPanel>
-                <h2>Any content 2</h2>
+                {categories}
                 </TabPanel>
             </Tabs>
             </div>
